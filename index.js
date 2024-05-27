8c9a93186184760671b24812dde843dba30975da
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2024-05-27 16:36:12 +0200

    Created home page. Added profile image and link to other related sites. Created header EJS file.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
