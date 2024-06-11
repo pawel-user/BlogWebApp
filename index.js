ffdefd108440ff02f1968e848799b9f78f93da36
Paweł Bochacik <pawel.bochacik.programista@gmail.com>
2024-06-11 09:29:49 +0200

    Created several sample routes in main file index.js. Added feature to preview and edit posts.

--- index.js
+++ index.js
@@ -6,22 +6,16 @@ const app = express();
 const port = 3000;
 
 var posts = [];
-var paths = [];
+var position = posts.length;
+var index = 0;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-
-for (var i=0; i<posts.length; i++) {
-  var path = paths[i];
-  app.get(`/${path}`, (req, res) => {
-    res.render(path + ".ejs", { article: posts[i] });
-  });
-};
 
 app.get("/bio", (req, res) => {
   res.render("bio.ejs");
@@ -48,29 +42,106 @@ app.post("/create-post", (req, res) => {
       day: "2-digit",
     }),
     htmlContent: req.body["content"],
+    endpoint: req.body["title"].replaceAll(" ", "").toLowerCase(),
+    position: position,
   };
-  var path = article.title.replaceAll(" ", "").toLowerCase();
   var data = `<%- include("partials/template.ejs") %>`;
 
   posts.push(article);
-  paths.push(path);
-  fs.writeFile(`./views/${path}.ejs`, data, (err) => {
+  position++;
+
+  console.log("position: " + position);
+  console.log(posts);
+  fs.writeFile(`./views/${article.endpoint}.ejs`, data, (err) => {
     if (err) throw err;
     console.log("The file has been saved!");
   });
 
-  //res.render(`${path}.ejs`, { article: article });
-  res.render("posts.ejs", { posts: posts });
+  res.render("posts.ejs", { posts: posts, position });
 });
 
 app.get("/create-post", (req, res) => {
-  res.render("create-post.ejs");
+  res.render("create-post.ejs", { posts });
 });
 
-app.get("/hellopeppa", (req,res) => {
-  res.render("hellopeppa.ejs", {article: posts[0]});
+app.get("/edit-post", (req, res) => {
+  res.render("edit-post.ejs", { posts, index });
+});
+
+app.post("/edit-post", (req, res) => {
+
+  var old_endpoint = posts[index].endpoint;
+
+  const article = {
+    title: req.body["title"],
+    date: new Date().toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+    }),
+    htmlContent: req.body["content"],
+    endpoint: req.body["title"].replaceAll(" ", "").toLowerCase(),
+    position: index,
+  };
+
+  var data = `<%- include("partials/template.ejs") %>`;
+
+  posts[article.position] = article;
+  console.log(posts);
+
+
+  fs.writeFile(`./views/${article.endpoint}.ejs`, data, (err) => {
+    if (err) throw err;
+    console.log(`The new file ${article.endpoint}.ejs has been saved!`);
+  });
+
+  if (old_endpoint != article.endpoint) {
+    fs.unlink(`./views/${old_endpoint}.ejs`, (err) => {
+      if (err) throw err;
+      console.log(`The old file views/${old_endpoint}.ejs was deleted`);
+    });
+  }
+
+  res.render("posts.ejs", { posts: posts, position });
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
+});
+
+// A few route examples
+app.get("/postname", (req, res) => {
+
+  for (var i = 0; i < posts.length; i++) {
+    if (posts[i].endpoint === "postname") {
+      index = i;
+    }
+  }
+
+  console.log(posts[index]);
+  res.render("postname.ejs", { article: posts[index] });
+});
+
+app.get("/article", (req, res) => {
+
+  for (var i = 0; i < posts.length; i++) {
+    if (posts[i].endpoint === "article") {
+      index = i;
+    };
+  };
+
+  console.log(posts[index]);
+  res.render("article.ejs", { article: posts[index] });
+});
+
+app.get("/example", (req, res) => {
+
+  for (var i = 0; i < posts.length; i++) {
+    if (posts[i].endpoint === "example") {
+      index = i;
+    };
+  };
+  
+  console.log(posts[index]);
+  res.render("example.ejs", { article: posts[index] });
 });
